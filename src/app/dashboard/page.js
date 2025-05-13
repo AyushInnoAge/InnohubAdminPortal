980075e9698b9fc376c45a088889527a4fd81747
AyushInnoage30 <ayush.singh@innoage.in>
2025-05-13 11:15:38 +0530

    fix: remove unnecessary console log in fetchPosts function in Dashboard component

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -46,7 +46,6 @@ export default function Home() {
     try {
       const res = await DashboardDataFetch(lastFetchedDate);
       const response = res.message.dashboardData;
-      console.log("Dashboard Data", response);
 
       if (res.message.currentUserAchievements.length > 0) {
         setAchievements(res.message.currentUserAchievements);
